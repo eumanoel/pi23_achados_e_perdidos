0155db24f75d497f799024bf3a146bf23e78a94a
eumanoel <edjalma.sousa@escolar.ifrn.edu.br>
2023-12-22 13:19:08 -0300

    atu6

--- cadastro.php
+++ cadastro.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../pi23_achados_e_perdidos/css/login.css">
     <title>Document</title>
 </head>
 <body>
-    
+        <section class="area-login">
+            <div class="login">
+                <div>
+                    <img src="../pi23_achados_e_perdidos/imagens/coapac.png" alt="login" >
+                </div>
+                <form method="post" action="lcs.html">
+                    <input type="text" name="nome" placeholder="Nome de usuário" autofocus>
+                    <input type="email" name="email" placeholder="E-mail">
+                    <input type="password" name="senha" placeholder="Senha">
+
+                    <div class="botoes">
+                    <button class="botao">Cadastrar-se</button>
+                    </div>
+                </form>
+            </div>
+        </section>
 </body>
 </html>
