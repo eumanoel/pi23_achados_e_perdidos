96578658923001d5b82ab210aac392066fb4ec70
eumanoel <edjalma.sousa@escolar.ifrn.edu.br>
2023-12-26 08:13:48 -0300

    atu12

--- bases/menu_2.php
+++ bases/menu_2.php
@@ -1,5 +1,8 @@
 <header><nav>
                    <ul>
+                      <img src="../pi23_achados_e_perdidos/imagens/IFRN.png" alt="">
+                   </ul>
+                   <ul>
                        <button> <a href="login.php">Login</a> </button>
                    </ul>
                    <ul>
