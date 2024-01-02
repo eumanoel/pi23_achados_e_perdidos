924c99cf13f7fac4e254dbc3002a7dc34afc8858
eumanoel <edjalma.sousa@escolar.ifrn.edu.br>
2024-01-02 13:35:49 -0300

    atu20

--- p_tipo_objeto.php
+++ p_tipo_objeto.php
@@ -45,8 +45,8 @@ if ($result->num_rows > 0) {
 while($row = $result->fetch_assoc()) {
   echo "<tr>";
   echo "<td scope='row'>".$row["nome"]."</td>";
-  echo "<td><a href='p_areas_editar.php?id=".$row["id"]."'>EDITAR</td>";
-  echo "<td><a href='php/p_area_remover_bd.php?id=".$row["id"]."'>REMOVER</td>";
+  echo "<td><a href='p_tipo_objeto_editar.php?id=".$row["id"]."'>EDITAR</td>";
+  echo "<td><a href='php/p_tipo_objeto_remover_bd.php?id=".$row["id"]."'>REMOVER</td>";
   echo "</tr>";
 }
 } else {
@@ -72,7 +72,6 @@ desconectar($conn);
 </div>
 
 
- <!-- INCLUINDO CÓDIGO DE RODAPÉ COMUM A TODAS AS PÁGINAS -->
  <?php include 'bases/rodape_2.php'; ?>  
 </body>
 </html>
